3691e1ba8eaf06b711b88b9c0482649d02b04803
RenattoKstro <renatto.kstro@gmail.com>
2025-10-08 13:50:36 -0300

    Update CalculatorPage.tsx

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,7 +1,11 @@
 import Calculator from "@/components/Calculator";
 
 const CalculatorPage = () => {
-  return <Calculator />;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <Calculator />
+    </div>
+  );
 };
 
 export default CalculatorPage;
