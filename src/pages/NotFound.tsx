e3f6f7ab2cdd8a8118c97c3e87dbd061c8f0c803
RenattoKstro <renatto.kstro@gmail.com>
2025-10-13 11:46:33 -0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -14,7 +14,7 @@ const NotFound = () => {
         <h1 className="mb-4 text-4xl font-bold">404</h1>
         <p className="mb-4 text-xl text-gray-600">Oops! Pagina não encontrada</p>
         <a href="/" className="text-blue-500 underline hover:text-blue-700">
-          Return to Home
+          Ir à página principal
         </a>
       </div>
     </div>
